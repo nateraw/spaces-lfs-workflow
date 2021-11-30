50a1cad4b1d79df5199e87f030edf796ca4c74fc
nateraw <nxr9266@g.rit.edu>
2021-11-30 16:59:35 -0500

    :tada: init

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+
+import streamlit as st
+from huggingface_hub import Repository
+
+
+st.title("Spaces LFS Workflow")
+
+if os.environ.get("DO_DOWNLOAD_CACHE") and 'cache_is_downloaded' not in st.session_state:
+    with st.spinner("Downloading cache..."):
+        repo = Repository(local_dir="cache_dir", clone_from='nateraw/fairface', repo_type='dataset')
+        st.write(os.listdir('.'))
+        st.write(os.listdir('./cache_dir'))
+
