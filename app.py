7e5fab73cc857435707b0fa9928378065043b6c8
nateraw <nxr9266@g.rit.edu>
2021-11-30 17:35:35 -0500

    :art: cleanup

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 from subprocess import call
 
@@ -6,7 +7,11 @@ import streamlit as st
 from huggingface_hub import Repository
 
 
+CACHE_DIR = 'cache_dir/'
+
+
 def run_cmd(command):
+    """Runs CLI commands from Python, with outputs printed to shell"""
     try:
         print(command)
         call(command, shell=True)
@@ -15,12 +20,18 @@ def run_cmd(command):
         sys.exit(1)
 
 
-st.title("Spaces LFS Workflow")
+def download_cache(cache_dir: str, repo_name: str):
+    """Clones a repo from HuggingFace Hub to a cache directory"""
+    if os.environ.get("DO_DOWNLOAD_CACHE") and 'cache_is_downloaded' not in st.session_state:
+        with st.spinner("Downloading cache..."):
+            repo = Repository(local_dir=cache_dir, clone_from=repo_name, repo_type='dataset')
+            # repo.git_pull()  # Needed?
 
-if os.environ.get("DO_DOWNLOAD_CACHE") and 'cache_is_downloaded' not in st.session_state:
-    with st.spinner("Downloading cache..."):
-        repo = Repository(local_dir="cache_dir", clone_from='nateraw/fairface', repo_type='dataset')
 
-st.write(os.listdir('.'))
-st.write(os.listdir('./cache_dir'))
-run_cmd('ls -lash cache_dir/')
+def main():
+    st.title("Spaces LFS Workflow")
+    download_cache('cache_dir', 'nateraw/fairface')
+    st.write(os.listdir('.'))
+    st.write(os.listdir('./cache_dir'))
+    st.json(os.environ)
+    run_cmd('ls -lash cache_dir/')
