1d39d61b92b4f1a1a69a24bbbbd3da1e62707840
nateraw <nxr9266@g.rit.edu>
2021-11-30 17:05:11 -0500

    :loud_sound: update logs

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import os
+import sys
+from subprocess import call
 
 import streamlit as st
 from huggingface_hub import Repository
+
+
+def run_cmd(command):
+    try:
+        print(command)
+        call(command, shell=True)
+    except KeyboardInterrupt:
+        print("Process interrupted")
+        sys.exit(1)
 
 
 st.title("Spaces LFS Workflow")
@@ -12,3 +23,4 @@ if os.environ.get("DO_DOWNLOAD_CACHE") and 'cache_is_downloaded' not in st.sessi
 
 st.write(os.listdir('.'))
 st.write(os.listdir('./cache_dir'))
+run_cmd('ls -lash cache_dir/')
