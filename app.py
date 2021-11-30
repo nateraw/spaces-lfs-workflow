fb7e2da95148b53621690db94fa197b8c77cb38f
nateraw <nxr9266@g.rit.edu>
2021-11-30 17:02:29 -0500

    :construction: wip

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ st.title("Spaces LFS Workflow")
 if os.environ.get("DO_DOWNLOAD_CACHE") and 'cache_is_downloaded' not in st.session_state:
     with st.spinner("Downloading cache..."):
         repo = Repository(local_dir="cache_dir", clone_from='nateraw/fairface', repo_type='dataset')
-        st.write(os.listdir('.'))
-        st.write(os.listdir('./cache_dir'))
 
+st.write(os.listdir('.'))
+st.write(os.listdir('./cache_dir'))
